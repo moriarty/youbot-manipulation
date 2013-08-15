c0415a43efb05d20943f9e54bf550e35c4d8b730
Frederik Hegger <frederik.hegger@h-brs.de>
2013-08-15 13:32:15 +0200

    remove unused variable

--- youbot_joint_trajectory_action/ros/scripts/joint_trajectory_action.py
+++ youbot_joint_trajectory_action/ros/scripts/joint_trajectory_action.py
@@ -48,7 +48,6 @@ class JointTrajectoryAction:
 	def execute_cb(self, goal):
 		is_timed_out = False
 		start = rospy.Time.now()
-		duration = goal.trajectory.points[(len(goal.trajectory.points) - 1)].time_from_start + rospy.Duration(30.0)
 		
 		for i in range(len(goal.trajectory.points)):
 			joint_positions = brics_actuator.msg.JointPositions()
